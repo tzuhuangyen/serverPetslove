7a78b838ddcb9f4667786e02ad4970e1d4bd64fe
tzuhuangyen <912yan@gmail.com>
2024-10-02 22:58:49 +0200

    addItemToServe

--- routes/usersMemberRoutes.js
+++ routes/usersMemberRoutes.js
@@ -248,7 +248,12 @@ router.post(
     });
 
     await userCart.save(); // 保存購物車
-    res.status(200).json({ success: true, data: cart }); // 返回更新後的購物車
+    res.status(200).json({
+      success: true,
+      data: {
+        cart: userCart,
+      }, // 返回更新後的購物車
+    });
   })
 );
 
